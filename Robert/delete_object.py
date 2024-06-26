b99ec299724ac818cdb69ee01b1740bbcfe46d6e
0ld Camel <0ldcamel502@gmail.com>
2024-06-26 23:37:25 +0300

    rearrange functions in delete_object.py

--- Robert/delete_object.py
+++ Robert/delete_object.py
@@ -3,10 +3,28 @@ import time
 
 def main(): 
     canvas = Canvas(400, 400)
+    canvas.set_canvas_background_fill('white')
     canvas.create_text(200, 350, "Click to Continue", anchor='center', font_size = 24, color='blue')
     test_list(canvas)
     test_tuple(canvas)
     
+def create_list(canvas):
+    '''
+    return a list of two objects
+    '''
+    colliding_list = []
+    for i in range(2):
+        list_obj = canvas.create_rectangle(100, 100 + 100 * i, 150, 150 + 100 * i, 'red')
+        colliding_list.append(list_obj)
+    return colliding_list
+
+def create_tuple(canvas):
+    '''
+    return a tuple of two objects
+    '''
+    tuple_obj_1 = canvas.create_oval(100, 100, 150, 150, 'blue')
+    tuple_obj_2 = canvas.create_oval(100, 200, 150, 250, 'blue')
+    return tuple_obj_1, tuple_obj_2
 
 def test_list(canvas):
     colliding_list = create_list(canvas)
@@ -38,23 +56,6 @@ def test_tuple(canvas):
     canvas.create_text(200, 200, "Done with tuple", anchor='center', font_size = 24, color='green')
     canvas.wait_for_click()
 
-def create_list(canvas):
-    '''
-    return a list of two objects
-    '''
-    colliding_list = []
-    for i in range(2):
-        list_obj = canvas.create_rectangle(100, 100 + 100 * i, 150, 150 + 100 * i, 'red')
-        colliding_list.append(list_obj)
-    return colliding_list
-
-def create_tuple(canvas):
-    '''
-    return a tuple of two objects
-    '''
-    tuple_obj_1 = canvas.create_oval(100, 100, 150, 150, 'blue')
-    tuple_obj_2 = canvas.create_oval(100, 200, 150, 250, 'blue')
-    return tuple_obj_1, tuple_obj_2
 
 if __name__ == '__main__':
     main()
