dc89ee79abcbc9c949f5f3ff330e398da438f57b
0ld Camel <0ldcamel502@gmail.com>
2024-06-27 00:07:34 +0300

    update delete_object.py

--- Robert/delete_object.py
+++ Robert/delete_object.py
@@ -15,7 +15,10 @@ def create_list(canvas):
     colliding_list = []
     for i in range(2):
         list_obj = canvas.create_rectangle(100, 100 + 100 * i, 150, 150 + 100 * i, 'red')
+        print('list object', list_obj)
+        obj_id = canvas.create_text(250, 125 + 100 * i, text=f'object id {list_obj}', anchor='center', font_size=24)
         colliding_list.append(list_obj)
+        colliding_list.append(obj_id)
     return colliding_list
 
 def create_tuple(canvas):
@@ -23,8 +26,10 @@ def create_tuple(canvas):
     return a tuple of two objects
     '''
     tuple_obj_1 = canvas.create_oval(100, 100, 150, 150, 'blue')
+    obj_id_1 = canvas.create_text(250, 125, text=f'object id {tuple_obj_1}', anchor='center', font_size=24)
     tuple_obj_2 = canvas.create_oval(100, 200, 150, 250, 'blue')
-    return tuple_obj_1, tuple_obj_2
+    obj_id_2 = canvas.create_text(250, 225, text=f'object id {tuple_obj_2}', anchor='center', font_size=24)
+    return tuple_obj_1, obj_id_1, tuple_obj_2, obj_id_2
 
 def test_list(canvas):
     colliding_list = create_list(canvas)
@@ -50,7 +55,7 @@ def test_tuple(canvas):
     canvas.wait_for_click()
 
     for obj in colliding_tuple:
-        canvas.delete(obj)
+        canvas.delete(obj) # delete oval
         canvas.wait_for_click()
 
     canvas.create_text(200, 200, "Done with tuple", anchor='center', font_size = 24, color='green')
